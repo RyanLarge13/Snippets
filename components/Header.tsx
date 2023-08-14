58e0d4d67f149d8bb5d223a8890889f51f3e45f3
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-08-14 11:12:37 -0700

    Removing Register

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,57 +6,37 @@ import Image from "next/image";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
-  const [register, setRegister] = useState(false);
   const [signin, setSignIn] = useState(false);
 
   return (
     <header className="flex justify-between items-center px-3 py-5 bg-slate-700 shadow-sm bg-opacity-10">
-        <Image
-          src="/assets/images/logo.svg"
-          alt="logo"
-          width={30}
-          height={30}
+      <Image
+        src="/assets/images/logo.svg"
+        alt="logo"
+        width={30}
+        height={30}
+        className="shadow-md shadow-purple-500 rounded-full"
+      />
+      <div className="flex justify-center items-center">
+        <button
+          className={`${
+            signin ? "shadow-purple-500 shadow-sm" : "shadow-md"
+          } rounded-full py-1 px-3 bg-black bg-opacity-30 mr-5`}
+          onClick={() => {
+            open && setOpen(false);
+            setSignIn((prev) => !prev);
+          }}
+        >
+          Sign In
+        </button>
+        <CgMenuRight
+          onClick={() => {
+            signin && setSignIn(false);
+            setOpen((prev) => !prev);
+          }}
+          className="text-white text-xl cursor-pointer"
         />
-      <ul className="flex justify-end items-center">
-        <li className="mx-1">
-          <button
-            className={`${
-              signin ? "shadow-sm shadow-white" : "shadow-md"
-            } rounded-full bg-black py-1 px-3 bg-opacity-30`}
-            onClick={() => {
-              setRegister(false);
-              setOpen(false);
-              setSignIn((prev) => !prev);
-            }}
-          >
-            Signin
-          </button>
-        </li>
-        <li>
-          <button
-            className={`${
-              register ? "shadow-white shadow-sm" : "shadow-md"
-            } rounded-full py-1 px-3 bg-black bg-opacity-30 mr-5`}
-            onClick={() => {
-              open && setOpen(false);
-              signin && setSignIn(false);
-              setRegister((prev) => !prev);
-            }}
-          >
-            Register
-          </button>
-        </li>
-        <li>
-          <CgMenuRight
-            onClick={() => {
-              register && setRegister(false);
-              signin && setSignIn(false);
-              setOpen((prev) => !prev);
-            }}
-            className="text-white text-xl cursor-pointer"
-          />
-        </li>
-      </ul>
+      </div>
       {open && (
         <AnimatePresence>
           <motion.nav
@@ -72,7 +52,7 @@ const Header = () => {
           </motion.nav>
         </AnimatePresence>
       )}
-      {register && (
+      {signin && (
         <AnimatePresence>
           <motion.div
             initial={{ top: "110%" }}
@@ -83,7 +63,7 @@ const Header = () => {
             }}
             className="fixed top-[-50px] right-10 left-10 bg-gradient-to-tr from-black to-purple-900 bottom-10 rounded-md shadow-sm z-10 p-3 overflow-y-auto"
           >
-            <h2>Register for a new account</h2>
+            <h2>Sign in or sign up</h2>
             <form className="bg-black bg-opacity-30 rounded-md shadow-md p-3 mt-10 mb-5 text-white">
               <input
                 className="p-3 rounded-md shadow-md bg-white bg-opacity-40 w-full"
@@ -101,26 +81,11 @@ const Header = () => {
                 type="submit"
                 className="bg-black bg-opacity-30 rounded-md shadow-md mt-3 px-3 py-1"
               >
-                Register
+                Sign in
               </button>
             </form>
             <hr />
             <p className="mt-2 text-center">or</p>
-          </motion.div>
-        </AnimatePresence>
-      )}
-      {signin && (
-        <AnimatePresence>
-          <motion.div
-            initial={{ top: "110%" }}
-            exit={{ top: "110%" }}
-            animate={{
-              top: "25vh",
-              transition: { type: "spring", stiffness: 50 },
-            }}
-            className="fixed top-[-50px] right-10 left-10 bg-gradient-to-tr from-black to-purple-900 bottom-10 rounded-md shadow-sm z-10 p-3"
-          >
-            <h2>Signup and join the community</h2>
           </motion.div>
         </AnimatePresence>
       )}
